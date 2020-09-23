0742b4fda0b72fe4d2f9e562b787ac8728b20197
matthew stewardson <mstewardson@myseneca.ca>
2020-09-23 13:32:07 -0400

    Worked and added new features such as colour.

--- LinkStatus.go
+++ LinkStatus.go
@@ -1,4 +1,3 @@
-
 /*
 Name:    Matthew Stewardson
 Date:    23-09-20
@@ -9,19 +8,21 @@ Extra:   This is my first time using GO so I'm leaving comments to explain certa
 package main
 
 import (
-    "fmt"		
-    "log"		
-	"net/http"	
+	"fmt"
 	"io/ioutil"
-	"regexp"
-	flag "github.com/spf13/pflag"		//Using pflag here for added functionality compared to flag
+	"log"
+	"net/http"
 	"os"
+	"regexp"
+
+	"github.com/fatih/color"
+	flag "github.com/spf13/pflag" //Using pflag here for added functionality compared to flag
 )
 
 /*
 Opens and reads the given file into a single string. This string is then
 checked for url's via a regex pattern.
-*/ 
+*/
 func readFile(file string) {
 	f, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -31,44 +32,60 @@ func readFile(file string) {
 	text := string(f)
 	re := regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 	result := re.FindAllString(text, -1)
-
-	for i := range(result) {
-		checkStatus(result[i], i)
+	fmt.Println("File output")
+	var temp = "test"
+	for i := range result {
+		//checkStatus(result[i], i)
+		if temp == result[i] {
+			//DO nothing its a dupe
+		} else {
+			checkStatus(result[i], i)
+		}
+		//fmt.Println(result[i])
+		temp = result[i]
 	}
 }
 
 //Does a GET request on the given string found within the file
 func checkStatus(url string, i int) {
-	i++
-	response, err := http.Get(url)		
 
-    if err != nil {
-        log.Fatal(err)																	
-    }
-    if response.StatusCode >= 200 && response.StatusCode <= 299 {
-		if i < 10 {
-			fmt.Println(i, " -> [GOOD]    " , response.StatusCode, "URL: ", url)
-		} else {
-			fmt.Println(i, "-> [GOOD]    " , response.StatusCode, "URL: ", url)
-		}
-	} else if response.StatusCode == 400{
-		if i < 10 {
-			fmt.Println(i, " -> [BAD]     " , response.StatusCode, "URL: ", url)
-		} else {
-			fmt.Println(i, "-> [BAD]     " , response.StatusCode, "URL: ", url)
-		}
-	} else if response.StatusCode == 404{
-		if i < 10 {
-			fmt.Println(i, " -> [BAD]     " , response.StatusCode, "URL: ", url)
-		} else {
-			fmt.Println(i, "-> [BAD]     " , response.StatusCode, "URL: ", url)
-		}
+	//Colours
+	c := color.New(color.FgCyan)
+	r := color.New(color.FgRed)
+	g := color.New(color.FgGreen)
+
+	i++
+	response, err := http.Get(url)
+
+	if err != nil {
+		r.Println(i, "-> [ERROR]   ", "URL: ", url)
 	} else {
-		if i < 10 {
-			fmt.Println(i, " -> [UNKNOWN] " , response.StatusCode, "URL: ", url)
+		if response.StatusCode >= 200 && response.StatusCode <= 299 {
+			if i < 10 {
+				g.Println(i, " -> [GOOD]    ", response.StatusCode, "URL: ", url)
+			} else {
+				g.Println(i, "-> [GOOD]    ", response.StatusCode, "URL: ", url)
+			}
+		} else if response.StatusCode == 400 {
+			if i < 10 {
+				r.Println(i, " -> [BAD]     ", response.StatusCode, "URL: ", url)
+			} else {
+				r.Println(i, "-> [BAD]     ", response.StatusCode, "URL: ", url)
+			}
+		} else if response.StatusCode == 404 {
+			if i < 10 {
+				r.Println(i, " -> [BAD]     ", response.StatusCode, "URL: ", url)
+			} else {
+				r.Println(i, "-> [BAD]     ", response.StatusCode, "URL: ", url)
+			}
 		} else {
-			fmt.Println(i, "-> [UNKNOWN] " , response.StatusCode, "URL: ", url)
+			if i < 10 {
+				c.Println(i, " -> [UNKNOWN]  URL: ", url)
+			} else {
+				c.Println(i, "-> [UNKNOWN]  URL: ", url)
+			}
 		}
+		defer response.Body.Close()
 	}
 }
 
@@ -79,7 +96,7 @@ func main() {
 	flag.Parse()
 
 	if *version == true {
-		fmt.Println("LinkStatus version 0.1.1")
+		fmt.Println("LinkStatus version 0.1.2")
 		return
 	}
 
