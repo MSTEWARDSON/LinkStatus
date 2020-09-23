118c321305ee85434954d9ad610c6a50eca1a352
matthew stewardson <mstewardson@myseneca.ca>
2020-09-23 01:42:22 -0400

    Can now read files and check urls from the files.

--- LinkStatus.go
+++ LinkStatus.go
@@ -1,37 +1,101 @@
 
 /*
 Name:    Matthew Stewardson
-Date:    22-09-20
-Version: 1.0.0
-Desc:    First iteration of my link checker project, so far you can give it a url
-		 and it well return the status of that url.
+Date:    23-09-20
+Version: 1.0.1
+Desc:    Second iteration of my link checker project. I added the ability to read from a given file, and check url's from the file
 Extra:   This is my first time using GO so I'm leaving comments to explain certain aspects of the code for reference.
 */
 package main
 
 import (
-    "fmt"		//Standard formatted I/O functions
-    "log"		//Simple logging package. (used for the log.fatal() function call)
-    "net/http"	//This package provides http client and server implementations. (used to request the status on the url)
+    "fmt"		
+    "log"		
+	"net/http"	
+	"io/ioutil"
+	"regexp"
+	flag "github.com/spf13/pflag"		//Using pflag here for added functionality compared to flag
+	"os"
 )
 
-//Simple http status checker
-func main() {
+/*
+Opens and reads the given file into a single string. This string is then
+checked for url's via a regex pattern.
+*/ 
+func readFile(file string) {
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    response, err := http.Get("https://httpstat.us")									//Get req on the given url
+	text := string(f)
+	re := regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
+	result := re.FindAllString(text, -1)
+
+	for i := range(result) {
+		checkStatus(result[i], i)
+	}
+}
+
+//Does a GET request on the given string found within the file
+func checkStatus(url string, i int) {
+	i++
+	response, err := http.Get(url)		
+
     if err != nil {
-        log.Fatal(err)																	//using log.Fatal to report on any incorrect url
+        log.Fatal(err)																	
     }
-
-    fmt.Println("HTTP Response Status:", response.StatusCode, http.StatusText(response.StatusCode))
-
     if response.StatusCode >= 200 && response.StatusCode <= 299 {
-        fmt.Println("HTTP Status is in the 200 - 299 range. It's good!")
+		if i < 10 {
+			fmt.Println(i, " -> [GOOD]    " , response.StatusCode, "URL: ", url)
+		} else {
+			fmt.Println(i, "-> [GOOD]    " , response.StatusCode, "URL: ", url)
+		}
 	} else if response.StatusCode == 400{
-		fmt.Println("HTTP Status is 400. It's not good!")
+		if i < 10 {
+			fmt.Println(i, " -> [BAD]     " , response.StatusCode, "URL: ", url)
+		} else {
+			fmt.Println(i, "-> [BAD]     " , response.StatusCode, "URL: ", url)
+		}
 	} else if response.StatusCode == 404{
-		fmt.Println("HTTP Status is 404. It's not good!")
+		if i < 10 {
+			fmt.Println(i, " -> [BAD]     " , response.StatusCode, "URL: ", url)
+		} else {
+			fmt.Println(i, "-> [BAD]     " , response.StatusCode, "URL: ", url)
+		}
 	} else {
-        fmt.Println("Unknown/Broken")
-    }
+		if i < 10 {
+			fmt.Println(i, " -> [UNKNOWN] " , response.StatusCode, "URL: ", url)
+		} else {
+			fmt.Println(i, "-> [UNKNOWN] " , response.StatusCode, "URL: ", url)
+		}
+	}
+}
+
+//Setting up the optional version command
+var version = flag.BoolP("version", "v", false, "prints out version info")
+
+func main() {
+	flag.Parse()
+
+	if *version == true {
+		fmt.Println("LinkStatus version 0.1.1")
+		return
+	}
+
+	if len(os.Args) == 1 {
+		fmt.Println(`
+Name: LinkStatus
+Usage: go run LinkStatus.go filenames
+Example: go run LinkStatus.go urls.txt
+Version: go run main.go -v or --version to check version.
+				   `)
+		os.Exit(-1)
+	}
+
+	fmt.Println("Checking files ", os.Args[1:])
+
+	for _, file := range os.Args[1:] {
+		readFile(file)
+	}
 }
