5633a7f9c1ae53780aab37a32dafbdcb242e3fb7
matthew stewardson <mstewardson@myseneca.ca>
2020-09-23 21:35:37 -0400

    Fixed some bugs

--- LinkStatus.go
+++ LinkStatus.go
@@ -56,11 +56,11 @@ func checkStatus(url string, i int) {
 	i++
 
 	//Timeout
-	h := &http.Client{
+	client := &http.Client{
 		Timeout: 7 * time.Second,
 	}
 
-	response, err := h.Get(url)
+	response, err := client.Get(url)
 
 	if err != nil {
 		r.Println(i, " -> [ERROR]   ", "URL: ", url)
