174fc82fdfcf5e89516c68c716309e9a984bd931
matthew stewardson <mstewardson@myseneca.ca>
2020-11-26 13:52:14 -0500

    Added new 300 status code test

--- request_test.go
+++ request_test.go
@@ -32,3 +32,17 @@ func Test404Response(t *testing.T) {
 
 	checkStatus(mockServerURL, 1, false, 1)
 }
+
+//300 status code check
+func Test300Response(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `response from the mock server goes here`)
+		w.WriteHeader(http.StatusMultipleChoices)
+		w.Write([]byte("300 - Multiple choices!"))
+	}))
+	defer ts.Close()
+
+	mockServerURL := ts.URL
+
+	checkStatus(mockServerURL, 1, false, 1)
+}
